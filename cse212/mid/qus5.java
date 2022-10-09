1f796f21901d266d73a8e3f284157c015e1f01e0
Foysal Ahmed <39677289+younusFoysal@users.noreply.github.com>
2022-10-09 20:35:31 +0600

    Update qus5.java

--- cse212/mid/qus5.java
+++ cse212/mid/qus5.java
@@ -1,7 +1,58 @@
 
-
-
-Will be Added..... Working on it
-
-
-
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+    
+class BuildGUI extends JFrame implements ActionListener {
+        JFrame actualWindow;
+        JPanel container;
+        JTextField txt_num1, txt_num2, txt_result;
+        JButton btn_div;
+        
+        BuildGUI() {
+            actualWindow = new JFrame("Experiment 4");
+            container = new JPanel();
+            container.setLayout(new FlowLayout());
+            
+            txt_num1 = new JTextField(20);
+            txt_num2 = new JTextField(20);
+            txt_result = new JTextField(20);
+    
+            btn_div = new JButton("Divide");
+            btn_div.addActionListener(this);
+            
+            container.add(txt_num1);
+            container.add(txt_num2);
+            container.add(btn_div);
+            container.add(txt_result);
+            
+            actualWindow.add(container);
+            actualWindow.setSize(300, 300);
+            actualWindow.setVisible(true);
+        }
+    
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            int num1, num2;
+            try {
+                num1 = Integer.parseInt(txt_num1.getText());
+                num2 = Integer.parseInt(txt_num2.getText());
+                txt_result.setText(num1/num2+"");
+            }
+            catch(NumberFormatException nfe) {
+                JOptionPane.showMessageDialog(actualWindow,"Please do enter only integers");  
+            }
+            catch(ArithmeticException ae) {
+                JOptionPane.showMessageDialog(actualWindow,"Divisor can not be ZERO");  
+            }
+        }
+}
+public class Experiment_4 {
+    
+    public static void main(String[] args) {
+        new BuildGUI();
+    
+    }
+    
+}
+    
