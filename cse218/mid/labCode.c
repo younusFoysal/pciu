59a5d393d7984991f1305c806fa8b0903d1b49f2
Foysal Ahmed <39677289+younusFoysal@users.noreply.github.com>
2022-10-09 20:48:08 +0600

    Create labCode.c

---
+++ cse218/mid/labCode.c
@@ -0,0 +1,99 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the size of array : ");
+    scanf("%d",&n);
+    int a[n],i;
+    printf("Enter the elements of array :\n");
+    for(i=1; i<=n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf(" elements of array :");
+    for(i=1; i<=n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+    insert(a,n);
+    deleteValue(a,n);
+    bubble(a,n);
+    return 0;
+}
+void insert(int a[],int n)
+{
+    int insrt,temp,i;
+    printf("\nEnter inserting value: ");
+    scanf("%d",&insrt);
+
+    for(i=1; i<=n; i++)
+    {
+        if(a[i]>insrt)
+        {
+            temp = i;
+            break;
+        }
+    }
+    for(i=n; i>=temp; i--)
+    {
+        a[i+1] = a[i];
+    }
+    a[temp] = insrt;
+    printf("\nAfter inserting value: ");
+    for(i=1; i<=n+1; i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+void deleteValue(int a[],int n)
+{
+    int delValue,i,temp;
+    printf("\nEnter deleting value: ");
+    scanf("%d",&delValue);
+
+    for(i=1; i<=n; i++)
+    {
+        if(a[i]==delValue)
+        {
+            temp = i;
+        }
+    }
+    for(i=temp; i<=n; i++)
+    {
+        a[i] = a[i+1];
+    }
+
+    printf("\nAfter Deleting value: ");
+    for(i=1; i<=n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+void bubble(int a[],int n)
+{
+    int i,j,temp;
+    for(i=0; i<=n; i++)
+    {
+        for(j=1; j<=n-i-1; j++)
+        {
+            if(a[j]<a[j+1])
+            {
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
+            }
+        }
+    }
+    printf("\nbubble sort: ");
+    for(i=1; i<=n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+/*
+input:
+5
+1 2 4 5 6
+3
+4
+*/
